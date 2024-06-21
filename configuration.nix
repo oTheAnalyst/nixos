09561d40d2700728e020fb0dbea07b1b3b07c308
VtheRtech <sakeyisu@gmail.com>
2024-06-21 18:06:35 -0400

    fix some errors

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,5 @@
 # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# your system. Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
@@ -11,106 +11,96 @@
     ];
 
   hardware.opengl = {
-   enable = true;
-   driSupport = true;
-   driSupport32bit = true;
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
   };
-services.xserver.videoDrivers = ["amdgpu"];
+  
+  services.xserver.videoDrivers = ["amdgpu"];
 
+  # Install and enable Steam 
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
 
-# install and enable steam 
-  programs.steam.enable = true;
-  programs.steam.gamescopeSession.enable = true;
-  enviroment.systemPackages = with pkg; [
-    mangohud
-  ];
+  programs.gamemode.enable = true;
 
-programs.gamemode.enable = true;
-
-
-  # Bootloader.
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  # Networking settings
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    networkmanager.enable = true; # Enable networking
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  # Set your time zone
+  time.timeZone = "America/New_York";
 
-  # Enable the Cinnamon Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.cinnamon.enable = true;
+  # Select internationalisation properties
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
 
-  # Configure keymap in X11
+  # Enable the X11 windowing system
   services.xserver = {
+    enable = true;
+
+    # Enable the Cinnamon Desktop Environment
+    displayManager.lightdm.enable = true;
+    desktopManager.cinnamon.enable = true;
+
+    # Configure keymap in X11
     layout = "us";
     xkbVariant = "";
   };
 
-  # Enable CUPS to print documents.
+  # Enable CUPS to print documents
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
+  # Enable sound with PipeWire
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # Uncomment if you want to use JACK applications
+    # jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Define a user account
   users.users.pretender = {
     isNormalUser = true;
     description = "pretender";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
+      # Add user-specific packages here if needed
     ];
   };
 
-  # Install firefox.
+  # Install Firefox
   programs.firefox.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile
   environment.systemPackages = with pkgs; [
+    mangohud
     protonup
     vesktop
     neovim 
@@ -122,31 +112,10 @@ programs.gamemode.enable = true;
     bitwarden-desktop
   ];
 
- # protonup variable
+  # Environment session variables
   environment.sessionVariables = {
-  STEAM_EXTRA_COMPAT_TOOL_PATHS = 
-  "/home/pretender/.steam/root/compatibilitytools.d";
-	  };
-  }
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    STEAM_EXTRA_COMPAT_TOOL_PATHS = "/home/pretender/.steam/root/compatibilitytools.d";
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -155,5 +124,4 @@ programs.gamemode.enable = true;
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
