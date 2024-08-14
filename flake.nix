9d2b19d92274a4f7775c844f5276db6d3f63273a
VtheRtech <sakeyisu@gmail.com>
2024-08-14 18:25:33 -0400

    Reapply "added in yazi to flake, this is properly added"
    This reverts commit f42228b42614ea8d545b3984bf6eeb551d0a53a4.

--- flake.nix
+++ flake.nix
@@ -9,6 +9,20 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+<<<<<<< HEAD
+<<<<<<< HEAD
+    yazi = {
+      url = "github:sxyazi/yazi";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    Vesktop = {
+      url = "github:Vencord/Vesktop";
+      inputs.nixpkgs.follows = "nixpkgs";
+      };
+=======
+>>>>>>> parent of 17aa700 (added in yazi to flake, this is properly added)
+=======
+>>>>>>> parent of 17aa700 (added in yazi to flake, this is properly added)
   };
 
   outputs = inputs @ {
